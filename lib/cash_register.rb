126b2bb2fc183b4358666ead5b2b99436c9ea901
Holden Altaffer <haltaffer@gmail.com>
2018-03-07 21:21:01 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total = @total + (price * quantity)
+  end
+
+  def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      @total = @total - (@discount * 10)
+      "After the discount, the total comes to $#{@total}."
+    end
+  end
+
+end
